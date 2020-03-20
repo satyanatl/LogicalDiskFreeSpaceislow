e393f6b6e4bee9ccb8389c334cb108abaddd8e6e
satyanatl <satan.kumar@gmail.com>
2020-03-20 05:42:14 -0400

    Made this version same as Warnign version

--- Error_LogicalDiskFreeSpaceislow_MMDDYYYY.ps1
+++ Error_LogicalDiskFreeSpaceislow_MMDDYYYY.ps1
@@ -1,6 +1,4 @@
-﻿ Param (
-    [Parameter(Mandatory = $true)]
-    [string]$ci_name,
+ Param (
     [Parameter(Mandatory = $true)]
     [string]$writeToFile,
     [Parameter(Mandatory = $true)]
@@ -11,7 +9,9 @@
 	$objFolder = $objShell.Namespace(0xA)
 	$temp = get-ChildItem "env:\TEMP"
 	$temp2 = $temp.Value
-	$swtools = "c:\SWTOOLS\"
+	$customfolders = @("C:\Users\kaushal.kumar.sharma\Test\customfolder\",
+                       "C:\Users\kaushal.kumar.sharma\Test\customfolder1\"
+                      )
 	$WinTemp = "c:\Windows\Temp\*"
 # Function to Write Output to Host/ Log to file(to be implemented later)
 Function WriteLog{
@@ -25,15 +25,18 @@ Function WriteLog{
 }
 
 Try{
-    # Remove files located in "c:\SWTOOLS\"
+    # Remove files located in "Customfolder"
 	WriteLog "Clearing Generic folder"
-	#Remove-Item -Recurse  "$swtools\*" -Force -Verbose
+    Foreach ($customfolder IN $customfolders){
+        Remove-Item -Recurse  "$customfolder\*" -Force -Verbose
+    }
+	
 
 # Remove temp files located in "C:\Users\USERNAME\AppData\Local\Temp"
 	WriteLog "Removing Junk files in $temp2."
 	Remove-Item -Recurse  "$temp2\*" -Force -Verbose
 
-#	Empty Recycle Bin # http://demonictalkingskull.com/2010/06/empty-users-recycle-bin-with-powershell-and-gpo/
+#	Empty Recycle Bin
 	WriteLog "Emptying Recycle Bin."
 	$objFolder.items() | %{ remove-item $_.path -Recurse -Confirm:$false}
 	
@@ -42,7 +45,7 @@ Try{
 	Remove-Item -Recurse $WinTemp -Force 
 	
 # Running Disk Clean up Tool 
-#	WriteLog "Finally now , Running Windows disk Clean up Tool"
+#	WriteLog "Running Windows disk Clean up Tool"
 #	cleanmgr /sagerun:1 | out-Null 
 #	
 #	$([char]7)
@@ -50,15 +53,15 @@ Try{
 #	$([char]7)
 #	Sleep 1 	
 	
-#	WriteLog "I finished the cleanup task,Bye Bye " 
-##### End of the Script ##### ad
+#	WriteLog "Cleanup task complete" 
 
-#Write-Output $fqdn"|"$script_abs_path"|"$script_abs_path_prevalidation
+
 Write-Output "true"
 }
 catch
 {
-    Write-Output "true"
+    Write-Output "false"
 }
 
 
+##### End of the Script #####
